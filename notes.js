b74daafe87ad36d9bd0da9f5fb07e26854ea8eea
merttsvnc <merttsvnc@gmail.com>
2023-11-18 19:59:19 +0900

    Remove duplicateNotes filter in addNote function

--- notes.js
+++ notes.js
@@ -7,7 +7,6 @@ const getNotes = () => {
 
 const addNote = (title, body) => {
   const notes = loadNotes()
-  const duplicateNotes = notes.filter((note) => note.title === title)
   const duplicateNote = notes.find((note) => note.title === title)
 
   if (!duplicateNote) {
