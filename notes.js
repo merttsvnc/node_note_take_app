16658c4f248b2bb7c2a59142fcd8f6a6bee99fd0
merttsvnc <merttsvnc@gmail.com>
2023-11-18 20:03:00 +0900

    Remove unused function getNotes()

--- notes.js
+++ notes.js
@@ -1,10 +1,6 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-const getNotes = () => {
-  return 'Your notes...'
-}
-
 const addNote = (title, body) => {
   const notes = loadNotes()
   const duplicateNote = notes.find((note) => note.title === title)
@@ -69,7 +65,6 @@ const readNotes = (title) => {
 }
 
 module.exports = {
-  getNotes,
   addNote,
   removeNote,
   listNotes,
