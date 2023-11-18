8a0764d729f728b71547df54a230dbae7c1967ab
merttsvnc <merttsvnc@gmail.com>
2023-11-18 20:02:11 +0900

    Add readNotes function to read a specific note

--- notes.js
+++ notes.js
@@ -56,9 +56,22 @@ const listNotes = () => {
   notes.forEach((note) => console.log(note.title))
 }
 
+const readNotes = (title) => {
+  const notes = loadNotes()
+  const note = notes.find((note) => note.title === title)
+
+  if (note) {
+    console.log(chalk.inverse(note.title))
+    console.log(note.body)
+  } else {
+    console.log(chalk.red.inverse('No note found!'))
+  }
+}
+
 module.exports = {
   getNotes,
   addNote,
   removeNote,
   listNotes,
+  readNotes,
 }
