39793aab64103347959fc9477798fbd14bd22d13
merttsvnc <merttsvnc@gmail.com>
2023-11-18 19:30:03 +0900

    Add notes.json and integrate notes functionality

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const yargs = require('yargs')
+const notes = require('./notes.js')
 
 // Customize yargs version
 yargs.version('1.1.0')
@@ -20,8 +21,7 @@ yargs.command({
     },
   },
   handler: function (argv) {
-    console.log('Title: ' + argv.title)
-    console.log('Body: ' + argv.body)
+    notes.addNote(argv.title, argv.body)
   },
 })
 
---
+++ notes.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+
+const getNotes = () => {
+  return 'Your notes...'
+}
+
+const addNote = (title, body) => {
+  const notes = loadNotes()
+  const duplicateNotes = notes.filter((note) => {
+    return note.title === title
+  })
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title,
+      body,
+    })
+    saveNotes(notes)
+    console.log('New note added!')
+  } else {
+    console.log('Note title taken!')
+  }
+}
+
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json')
+    const dataJSON = dataBuffer.toString()
+    return JSON.parse(dataJSON)
+  } catch (e) {
+    return []
+  }
+}
+
+const saveNotes = (notes) => {
+  const dataJSON = JSON.stringify(notes)
+  fs.writeFileSync('notes.json', dataJSON)
+}
+
+module.exports = {
+  getNotes,
+  addNote,
+}
